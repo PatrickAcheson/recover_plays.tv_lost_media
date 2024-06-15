d84574d712363fcdb4ff27939f22a56c344b8e84
bng <90014630+PatrickAcheson@users.noreply.github.com>
2024-06-15 02:06:05 +0100

    edits

--- scraper.py
+++ scraper.py
@@ -15,26 +15,47 @@ def download_video(video_url, save_path):
     else:
         print(f"Failed to download: {video_url}")
 
-save_dir = 'downloaded_videos'
-os.makedirs(save_dir, exist_ok=True)
 
-response = requests.get(url)
-if response.status_code == 200:
-    content = response.content
-    soup = BeautifulSoup(content, 'html.parser')
+def find_txt_file():
+
+    current_dir = os.getcwd()
     
-    for video in soup.find_all('source', type='video/mp4'):
-        video_url = video['src']
-        
-        if video_url.startswith('//'):
-            video_url = 'https:' + video_url
+    files = os.listdir(current_dir)
+    
+    for file in files:
+        if file.endswith('.txt'):
+            return file
 
-        if "/processed/" in video_url:
-            video_url = video_url.replace("/480.mp4", "/720.mp4")
+def parse_url(save_dir):
+    pass
+
+
+if "__main__" == __name__:
+    url_txt_file = find_txt_file()
+    save_dir = 'downloaded_videos'
+    os.makedirs(save_dir, exist_ok=True)
+
+    print(url_txt_file)
+
+    parse_url()
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.content
+        soup = BeautifulSoup(content, 'html.parser')
         
-        video_name = os.path.basename(video_url)
-        save_path = os.path.join(save_dir, video_name)
-        
-        download_video(video_url, save_path)
-else:
-    print(f"Failed to fetch the page: {url}")
+        for video in soup.find_all('source', type='video/mp4'):
+            video_url = video['src']
+            
+            if video_url.startswith('//'):
+                video_url = 'https:' + video_url
+
+            if "/processed/" in video_url:
+                video_url = video_url.replace("/480.mp4", "/720.mp4")
+            
+            video_name = os.path.basename(video_url)
+            save_path = os.path.join(save_dir, video_name)
+            
+            download_video(video_url, save_path)
+    else:
+        print(f"Failed to fetch the page: {url}")
