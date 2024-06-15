9b513a067878001ca07d62ad00b65035f90e966e
bng <90014630+PatrickAcheson@users.noreply.github.com>
2024-06-15 02:14:24 +0100

    pasted urls and cleaned

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+from urllib.parse import urlparse, urlunparse
 
 url = "https://web.archive.org/web/20191216031501/https://plays.tv/video/5975203c5add88ca21/ez-ace-"
 
@@ -17,45 +18,50 @@ def download_video(video_url, save_path):
 
 
 def find_txt_file():
-
     current_dir = os.getcwd()
-    
     files = os.listdir(current_dir)
     
     for file in files:
         if file.endswith('.txt'):
             return file
 
-def parse_url(save_dir):
-    pass
+def parse_url(url_txt_file):
+    cleaned_urls = set()
+    with open(url_txt_file, "r") as file:
+        for url in file:
+            cleaned_urls.add(remove_query_params(url))
+    return cleaned_urls
 
+
+def remove_query_params(url):
+    parsed_url = urlparse(url)
+    cleaned_url = parsed_url._replace(query="")
+    return urlunparse(cleaned_url)
 
 if "__main__" == __name__:
     url_txt_file = find_txt_file()
     save_dir = 'downloaded_videos'
     os.makedirs(save_dir, exist_ok=True)
+    cleaned_urls = parse_url(url_txt_file)
 
-    print(url_txt_file)
-
-    parse_url()
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        content = response.content
-        soup = BeautifulSoup(content, 'html.parser')
-        
-        for video in soup.find_all('source', type='video/mp4'):
-            video_url = video['src']
+    for url in cleaned_urls:
+        response = requests.get(url)
+        if response.status_code == 200:
+            content = response.content
+            soup = BeautifulSoup(content, 'html.parser')
             
-            if video_url.startswith('//'):
-                video_url = 'https:' + video_url
+            for video in soup.find_all('source', type='video/mp4'):
+                video_url = video['src']
+                
+                if video_url.startswith('//'):
+                    video_url = 'https:' + video_url
 
-            if "/processed/" in video_url:
-                video_url = video_url.replace("/480.mp4", "/720.mp4")
-            
-            video_name = os.path.basename(video_url)
-            save_path = os.path.join(save_dir, video_name)
-            
-            download_video(video_url, save_path)
-    else:
-        print(f"Failed to fetch the page: {url}")
+                if "/processed/" in video_url:
+                    video_url = video_url.replace("/480.mp4", "/720.mp4")
+                
+                video_name = os.path.basename(video_url)
+                save_path = os.path.join(save_dir, video_name)
+                
+                download_video(video_url, save_path)
+        else:
+            print(f"Failed to fetch the page: {url}")
